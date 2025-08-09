1a208c6fbd3b4e6d1be0875a9cdf797d46c6afe4
Mo Hajbabaei <h9edev@gmail.com>
2025-08-09 19:18:54 +0400

    feat: add routes

---
+++ server/http/init.go
@@ -0,0 +1,79 @@
+package http
+
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"github.com/hajbabaeim/hltchtodo/todo_item/abstraction"
+	"github.com/hajbabaeim/hltchtodo/todo_item/delivery"
+	"net/http"
+)
+
+type server struct {
+	todoUc abstraction.Usecase
+	router *gin.Engine
+}
+
+func NewServer(todoUsecase abstraction.Usecase) *server {
+	return &server{
+		todoUc: todoUsecase,
+		router: gin.Default(),
+	}
+}
+
+func (s *server) Run(ctx context.Context) error {
+	return nil
+}
+
+func (s *server) GetRouter() *gin.Engine {
+	return s.router
+}
+
+func (s *server) SetupRoutes() {
+	//s.router.GET("/health", s.healthCheck)
+	api := s.router.Group("/api/v1")
+	{
+		todo := api.Group("/todos")
+		{
+			todo.POST("", func(c *gin.Context) {
+				result, err := delivery.CreateTodoItem(c, s.todoUc)
+				if err != nil {
+					c.JSON(http.StatusInternalServerError, err.Error())
+					return
+				}
+				c.JSON(http.StatusOK, result)
+			})
+			todo.GET("/:id", func(c *gin.Context) {
+				result, err := delivery.GetTodoItem(c, s.todoUc)
+				if err != nil {
+					c.JSON(http.StatusInternalServerError, err.Error())
+					return
+				}
+				c.JSON(http.StatusOK, result)
+			})
+			todo.PUT("/:id", func(c *gin.Context) {
+				result, err := delivery.UpdateTodoItem(c, s.todoUc)
+				if err != nil {
+					c.JSON(http.StatusInternalServerError, err.Error())
+					return
+				}
+				c.JSON(http.StatusOK, result)
+			})
+			todo.DELETE("/:id", func(c *gin.Context) {
+				result, err := delivery.DeleteTodoItem(c, s.todoUc)
+				if err != nil {
+					c.JSON(http.StatusInternalServerError, err.Error())
+					return
+				}
+				c.JSON(http.StatusOK, result)
+			})
+			todo.GET("", func(c *gin.Context) {
+				result, err := delivery.ListItems(c, s.todoUc)
+				if err != nil {
+					c.JSON(http.StatusInternalServerError, err.Error())
+					return
+				}
+				c.JSON(http.StatusOK, result)
+			})
+		}
+	}
+}
