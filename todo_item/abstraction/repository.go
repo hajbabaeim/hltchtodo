3156e032a185505080ef179e8e7322fa74c837f6
Mo Hajbabaei <h9edev@gmail.com>
2025-08-09 19:17:55 +0400

    refactor: get & list repo

--- todo_item/abstraction/repository.go
+++ todo_item/abstraction/repository.go
@@ -1,14 +1,15 @@
 package abstraction
 
 import (
+	"github.com/google/uuid"
 	"github.com/hajbabaeim/hltchtodo/todo_item/domain"
 )
 
 type Repository interface {
 	CreateItem(item *domain.TodoItem) error
 	GetItemByID(id uint64) (*domain.TodoItem, error)
-	GetItemByUUID(uid string) (*domain.TodoItem, error)
+	GetItemByUUID(uid uuid.UUID) (*domain.TodoItem, error)
 	UpdateItem(item *domain.TodoItem) error
-	DeleteItem(id uint64) error
+	DeleteItem(id uuid.UUID) error
 	ListItems() ([]*domain.TodoItem, error)
 }
